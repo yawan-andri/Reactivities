fa9882d0eae8f77ee479ee53db2338a52c6865c5
Andri <yawan.andrisetyawan@gmail.com>
2025-09-27 20:59:36 +0700

    Part 3 : Building Walking Skeleton part 2 - React Chapter 22

---
+++ client/src/App.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react"
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    fetch('https://localhost:5000/api/activities')
+      .then(response => response.json())
+      .then(data => setActivities(data))
+
+      return () => {}
+  }, [])
+
+  return (
+    <div>
+      <h3 className="app" style={{color: 'red'}}>Reactivities</h3>
+      <ul>
+        {activities.map((activity) => (
+          <li key={activity.id}>{activity.title}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
