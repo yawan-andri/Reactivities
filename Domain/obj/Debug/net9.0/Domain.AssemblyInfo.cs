12f1f76b16153dce3838cd99ede928018d0b0698
Andri <yawan.andrisetyawan@gmail.com>
2025-10-25 19:22:45 +0700

    Section 11: Part 109

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b45ae572220fcf76f40adcaf787f93b63c9dc9bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac10ab99e95b33a950d9e705f12829e0aa856d00")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,34 +1,20 @@
 import { Box, Button, Paper, TextField, Typography } from "@mui/material";
-import type { FormEvent } from "react";
 import { useActivities } from "../../../lib/hooks/useActivities";
-import { useNavigate, useParams } from "react-router";
+import { useParams } from "react-router";
+import { useForm, type FieldValues } from "react-hook-form";
+import { useEffect } from "react";
 
 export default function ActivityForm() {
+  const { register, reset, handleSubmit} = useForm();
   const {id} = useParams();
   const {updateActivity, createActivity, activity, isLoadingACtivity} = useActivities(id);
-  const navigate = useNavigate();
 
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-  
-    const formData = new FormData(event.currentTarget);
+  useEffect(() => {
+    if (activity) reset(activity);
+  }, [activity, reset]);
 
-    const data: {[key: string]: FormDataEntryValue} = {}
-    formData.forEach((value, key) => {
-      data[key] = value;
-    });
-
-    if (activity) {
-      data.id = activity.id;
-      await updateActivity.mutateAsync(data as unknown as Activity);
-      navigate(`/activities/${activity.id}`);
-    } else {
-      createActivity.mutate(data as unknown as Activity, {
-        onSuccess: (id) => {
-          navigate(`/activities/${id}`);
-        }
-      });
-    }
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
   }
 
   if (isLoadingACtivity) return <Typography>Loading Activity...</Typography>
@@ -38,21 +24,27 @@ export default function ActivityForm() {
          <Typography variant="h5" gutterBottom color="primary">
             {activity ? 'Edit Activity' : 'Create Activity'}
          </Typography>
-         <Box component='form' onSubmit={handleSubmit} display='flex' flexDirection='column' gap={3}>
-            <TextField name='title' label='Title' defaultValue={activity?.title} />
-            <TextField name='description' label='Description' defaultValue={activity?.description}  multiline rows={3} />
-            <TextField name='category' label='Category' defaultValue={activity?.category}  />
-            <TextField name='date' label='Date' type="date"
+         <Box 
+          component='form' 
+          onSubmit={handleSubmit(onSubmit)} 
+          display='flex' 
+          flexDirection='column' 
+          gap={3}
+        >
+            <TextField {...register('title')} label='Title' defaultValue={activity?.title} />
+            <TextField {...register('description')} label='Description' defaultValue={activity?.description}  multiline rows={3} />
+            <TextField {...register('category')} label='Category' defaultValue={activity?.category}  />
+            <TextField {...register('date')} label='Date' type="date"
               defaultValue={
                 activity?.date 
                   ? new Date(activity.date).toISOString().split('T')[0] 
                   : new Date().toISOString().split('T')[0] 
               }  
             />
-            <TextField name='city' label='City' defaultValue={activity?.city} />
-            <TextField name='venue' label='Venue' defaultValue={activity?.venue} />
+            <TextField {...register('city')} label='City' defaultValue={activity?.city} />
+            <TextField {...register('venue')} label='Venue' defaultValue={activity?.venue} />
             <Box display='flex' justifyContent='end' gap={3}>
-                <Button color='inherit' onClick={() => navigate(`/activities/${id}`)} >Cancel</Button>
+                <Button color='inherit'>Cancel</Button>
                 <Button 
                   type="submit" 
                   color='success' 
